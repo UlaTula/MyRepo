cd5039b0d0adf6f4547a8abe3336c155cc988468
Snezana Berzina <snezana.berzina@knowledgeprice.com>
2022-06-18 16:10:15 +0300

    4 exercise from Lection 3

---
+++ UzdevumsPersona/UzdevumsPersona/Persona.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UzdevumsPersona
+{
+    public class Persona
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime BirhDate { get; set; }
+        public string Hobby { get; set; }
+        public bool IsMale { get; set; }   
+      
+    }
+}
---
+++ UzdevumsPersona/UzdevumsPersona/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Globalization;
+using UzdevumsPersona;
+
+Persona person = new Persona();
+
+Console.Write("Please enter name: ");
+person.Name = Console.ReadLine();
+
+Console.Write("Please enter surname: ");
+person.Surname = Console.ReadLine();
+
+Console.Write("Please enter date of birth usinf format dd.mm.yyyy: ");
+person.BirhDate = DateTime.ParseExact(Console.ReadLine(), "dd.mm.yyyy", CultureInfo.InvariantCulture);
+string format = "dd.mm.yyyy";
+DateTime now = DateTime.Now;
+int n = int.Parse(person.BirhDate.ToString("yyyy"));
+int yearsOld = now.Year - n;
+
+Console.Write("Please enter hobby: ");
+person.Hobby = Console.ReadLine();
+
+Console.Write("Please enter gender (0 - male and 1 - female): ");
+string genderString = Console.ReadLine();
+int genderNumber = int.Parse(genderString);
+int divGender = genderNumber % 2;
+person.IsMale = divGender == 0;
+
+
+Console.WriteLine($"Hello my name is {person.Name} {person.Surname} and I am {yearsOld}");
