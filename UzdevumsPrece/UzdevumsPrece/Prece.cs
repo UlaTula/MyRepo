777b3dcb966efd705429a1efe6945841697fac55
Snezana Berzina <snezana.berzina@knowledgeprice.com>
2022-06-18 16:09:08 +0300

    3 exercise from Lection 3

---
+++ UzdevumsPrece/UzdevumsPrece/Prece.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UzdevumsPrece
+{
+    public class Prece
+    {
+
+        public Prece(double lenght, double width, double height)
+        {
+            Lenght = lenght;
+            Width = width;
+            Height = height;
+        }
+        public double Lenght { get; }
+        public double Width { get; }
+        public double Height { get; }
+        public string Weight { get; set; }
+    }
+
+
+}
+
---
+++ UzdevumsPrece/UzdevumsPrece/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+using UzdevumsPrece;
+
+
+Prece prece = new Prece(23,34,45);
+
+Console.WriteLine($"Please enter weight for product with sizes {prece.Lenght} x {prece.Width} x {prece.Height}");
+
+prece.Weight = Console.ReadLine();
+
+Console.WriteLine($"Weight for product {prece.Lenght} x {prece.Width} x {prece.Height} is {prece.Weight} kg");
